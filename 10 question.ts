0e91672b3346d564b1c88056fbb53d9a528fb8a1
fiza langah <160244285+fizalangah@users.noreply.github.com>
2024-02-29 09:31:44 -0800

    Update 10 question.ts

--- 10 question.ts
+++ 10 question.ts
@@ -1,4 +1,4 @@
-//** */ QUESTION # 1;**
+//** */ QUESTION # 11;**
 //Basic Types: Define a variable myNumber and assign it the value 5.Declare another variable myString and assign it the value "Hello, TypeScript!".
 
 //     .....SOLUTION......
@@ -7,7 +7,7 @@ let myString1:string = "Hello, typescript !";
 
 console.log(myNumber,myString1);
 
-// ** QUESTION # 2; **
+// ** QUESTION # 12; **
 // Type Annotations: Write type annotations for the following variables:
 //NAME,AGE, ISSTUDENT,
 
@@ -18,7 +18,7 @@ let myAge:number = 22;
 let isStudent:boolean = true;
 console.log("myname = "+myName1 , ",", "myage = " +myAge ,",",  "isstudent = " +isStudent)
     
-//** */ QUESTION # 3;**
+//** */ QUESTION # 13;**
 // Type Inference: TypeScript automatically infers types. Declare a variable myArray and assign it an array of numbers.
 
 //   ........SOLUTION.......,
@@ -26,7 +26,7 @@ let myArray:number[ ] = [1,2,3,4,5,6,7,8,9,10];
 console.log(myArray);
 
 
-// **QUESTION # 4;**
+// **QUESTION # 14;**
 // Type Union: Create a function called printID that takes either a number or a string as an argument and prints it.
 
 ///    ......SOLUTION.....,
@@ -36,7 +36,7 @@ let printID = ((id:number|string):void=>{
  printID(6);
  printID("hello");
 
-//** */ QUESTION # 5;**
+//** */ QUESTION # 15;**
 // Interfaces: Define an interface Person with properties name of type string and age of type number.
 
 //     .....SOLUTION.......,
@@ -51,7 +51,7 @@ let nwepersonp:Person0 = {
 }
 console.log(nwepersonp)
 
-// ** Question # 6;**
+// ** Question # 16;**
 // Implementing Interfaces: Create a class Student that implements the Person interface. Add a method introduce that prints "Hi, my name is [name] and I'm [age] years old."
 
 //    ......SOLUTION......,
@@ -78,7 +78,7 @@ constructor(name2:string,age2:number){
   console.log(Student1.information());
 
 
-// ** QUESTION # 7** ;
+// ** QUESTION # 17** ;
 //  Access Modifiers: Add access modifiers to the name and age properties in the Person interface to make them read-only.
 
 //    ....SOLUTION.......,
@@ -104,7 +104,7 @@ information():void{
  console.log(Student.information());
 // Student.name3 = "fiza"; not possible;
     
-// **  QUESTION # 8 **;
+// **  QUESTION # 18 **;
 // Type Assertion: Convert a variable myAny of type any to a string type.
 
  let myAny:any = "bilo";
@@ -112,7 +112,7 @@ information():void{
  let myString:string = myAny as string;
 console.log(myString);
 
-//  **  QUESTION # 9; **
+//  **  QUESTION # 19; **
 
 // Function Types: Define a function type for a function that takes two numbers and returns their sum.;
 
@@ -125,7 +125,7 @@ const sum1 = sum(3,6);
    console.log(sum1);
  
 
-// ** QUESTION # 10 **;
+// ** QUESTION # 20 **;
 // Generics: Write a generic function called reverseArray that takes an array of any type and returns the reverse of that array.
 
 //   .....SOLUTION......,
